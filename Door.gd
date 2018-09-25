1853cdceb4ab6965d5b4fde40c337666271304fd
G-Man <wgkgarrett@gmail.com>
2018-09-25 00:38:13 -0600

    Add files via upload
    Working doors

--- Door.gd
+++ Door.gd
@@ -1,28 +1,29 @@
 extends Area2D
 
+
 var timer = null
 var can_close = false
 
 func _ready():
-	
+
 	timer = Timer.new()
-	timer.set_one_shot(false)
-	timer.set_wait_time(1) 
+	timer.set_one_shot(true)
+	timer.set_wait_time(1)
 	timer.connect("timeout", self, "on_timeout_complete")
 	add_child(timer)
 
 func on_timeout_complete():
 	can_close = true
 
-func _on_door_body_entered(body):
-	if body is preload("res://player.gd"):
-		print(body)
-		$anim.play("Open")
-	can_close = false
-	
-	timer.start()
-
 func _process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "player" && Input.is_key_pressed(KEY_E):
+			
+			$anim.play("Open")
+			can_close = false
+			timer.start()
+
 	if can_close == true:
 		$anim.play("Closed")
-		
+	
--- player.gd
+++ player.gd
@@ -6,24 +6,30 @@ const ACCELERATION = 25
 
 var motion = Vector2() # single unit used to store (x, y)
 
-func _physics_process(delta):
+var anim=""
 
-	if Input.is_action_pressed("ui_down"):
-		motion.y = min(motion.y+ACCELERATION, MAX_SPEED)
-	elif Input.is_action_pressed("ui_up"):
-		motion.y = max(motion.y-ACCELERATION, -MAX_SPEED)
+func _physics_process(delta):
+	
+
+	
+	if Input.is_action_pressed("ui_down"): #Is S pressed
+		motion.y = min(motion.y+ACCELERATION, MAX_SPEED) # accelerate down
+	elif Input.is_action_pressed("ui_up"):  # Is W pressed
+		motion.y = max(motion.y-ACCELERATION, -MAX_SPEED) # Accelerate up
 	
 	else:
-		motion.y = lerp(motion.y, 0, 0.2)
+		motion.y = lerp(motion.y, 0, 0.2) # Otherwise add friction in Y plane
 		
-	if Input.is_action_pressed("ui_right"):
-		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
+	if Input.is_action_pressed("ui_right"): # Is D pressed
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED) # accelerate right
+		
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 
 	else:
 		motion.x = lerp(motion.x, 0, 0.2)
-
+		
+	look_at(get_global_mouse_position())
 
 	motion = move_and_slide(motion)
 	pass
